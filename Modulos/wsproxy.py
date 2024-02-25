46766ca29df7417daa9eb2708a6812438afab82f
PLAZANETVPN <almundial22@gmail.com>
2024-02-25 17:18:03 -0500

    Update wsproxy.py

--- Modulos/wsproxy.py
+++ Modulos/wsproxy.py
@@ -265,8 +265,8 @@ def parse_args(argv):
 def main(host=LISTENING_ADDR, port=LISTENING_PORT):
     print("\033[0;34m━" * 8, "\033[1;32m PROXY WEBSOCKET", "\033[0;34m━" * 8, "\n")
     print("\033[1;33mIP:\033[1;32m " + LISTENING_ADDR)
-    print("\033[1;33mPORTA:\033[1;32m " + str(LISTENING_PORT) + "\n")
-    print("\033[0;34m━" * 10, "\033[1;32m TURBONET2023", "\033[0;34m━\033[1;37m" * 11, "\n")
+    print("\033[1;33mPUERTO:\033[1;32m " + str(LISTENING_PORT) + "\n")
+    print("\033[0;34m━" * 10, "\033[1;32m PLAZANET SSH", "\033[0;34m━\033[1;37m" * 11, "\n")
 
     server = Server(LISTENING_ADDR, LISTENING_PORT)
     server.start()
